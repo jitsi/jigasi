ea38a8df3a5d60d1fee7f7a5a9d44be29b55831e
Damian Minkov <damencho@jitsi.org>
2014-08-26 17:54:36 +0300

    Adds missing sh script, fixes native lib.

---
+++ jigasi.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+echo $SCRIPT_DIR
+
+mainClass="org.jitsi.jigasi.Main"
+cp=$(JARS=($SCRIPT_DIR/jigasi.jar $SCRIPT_DIR/lib/*.jar); IFS=:; echo "${JARS[*]}")
+libs="$SCRIPT_DIR/lib/native/linux"
+
+java -Djava.library.path=$libs -Djava.util.logging.config.file=lib/logging.properties -cp $cp $mainClass $@
