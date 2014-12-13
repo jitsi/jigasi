e6f1446f73a577c1b13def0c321b1707aca61b56
Boris Grozev <boris@jitsi.org>
2014-12-13 11:30:33 +0100

    Detect the architecture at runtime and use the correct native libraries directory.

--- jigasi.sh
+++ jigasi.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+kernel="$(uname -s)"
+if [ $kernel == "Darwin" ] ; then
+    SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+    architecture="mac"
+elif [ $kernel == "Linux" ] ; then
+    SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+    architecture="linux"
 
-#echo $SCRIPT_DIR
+    machine="$(uname -m)"
+    if [ "$machine" == "x86_64" ] || [ "$machine" == "amd64" ] ; then
+        architecture=$architecture"-64"
+    fi
+else
+    echo "Unknown system : $kernel"
+    exit 1
+fi
 
 mainClass="org.jitsi.jigasi.Main"
 cp=$(JARS=($SCRIPT_DIR/jigasi.jar $SCRIPT_DIR/lib/*.jar); IFS=:; echo "${JARS[*]}")
-libs="$SCRIPT_DIR/lib/native/linux"
+libs="$SCRIPT_DIR/lib/native/$architecture"
 
 java -Djava.library.path=$libs -Djava.util.logging.config.file=$SCRIPT_DIR/lib/logging.properties -cp $cp $mainClass $@
