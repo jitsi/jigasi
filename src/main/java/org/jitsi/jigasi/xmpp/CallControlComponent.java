f31e9b303219a01a3e313a6e4e200fcfbc9ce98f
damencho <damencho@jitsi.org>
2017-04-10 13:30:02 -0500

    Moves call control logic out of the component implementation.
    Having the logic separate can be reused when receiving controlling messages in other places, than only in components.

--- src/main/java/org/jitsi/jigasi/xmpp/CallControlComponent.java
+++ src/main/java/org/jitsi/jigasi/xmpp/CallControlComponent.java
@@ -17,15 +17,12 @@
  */
 package org.jitsi.jigasi.xmpp;
 
-import net.java.sip.communicator.impl.protocol.jabber.extensions.rayo.*;
 import net.java.sip.communicator.util.*;
 
 import org.jitsi.jigasi.*;
-import org.jitsi.jigasi.util.*;
 import org.jitsi.meet.*;
 import org.jitsi.service.configuration.*;
 import org.jitsi.xmpp.component.*;
-import org.jivesoftware.smack.packet.*;
 import org.osgi.framework.*;
 import org.xmpp.component.*;
 import org.xmpp.packet.IQ;
@@ -47,37 +44,9 @@ public class CallControlComponent
         = Logger.getLogger(CallControlComponent.class);
 
     /**
-     * Name of 'header' attribute that hold JVB room name.
+     * The call controlling logic.
      */
-    public static final String ROOM_NAME_HEADER = "JvbRoomName";
-
-    /**
-     * Optional header for specifying password required to enter MUC room.
-     */
-    public static final String ROOM_PASSWORD_HEADER = "JvbRoomPassword";
-
-    /**
-     * JID allowed to make outgoing SIP calls.
-     */
-    public static final String ALLOWED_JID_P_NAME
-        = "org.jitsi.jigasi.ALLOWED_JID";
-
-    /**
-     * The {@link SipGateway} service which manages gateway sessions.
-     */
-    private SipGateway gateway;
-
-    /**
-     * The only JID that will be allowed to create outgoing SIP calls. If not
-     * set then anybody is allowed to do so.
-     */
-    private String allowedJid;
-
-    /**
-     * FIXME: temporary to be removed/fixed
-     */
-    //private Map<SipGateway, String> hangupMap
-      //  = new HashMap<SipGateway, String>();
+    private CallControl callControl = null;
 
     /**
      * Creates new instance of <tt>CallControlComponent</tt>.
@@ -134,12 +103,12 @@ public class CallControlComponent
     public void start(BundleContext bundleContext)
         throws Exception
     {
-        this.gateway
+        SipGateway gateway
             = ServiceUtils.getService(
                     bundleContext, SipGateway.class);
 
-        if (this.gateway != null)
-            internalStart(bundleContext);
+        if (gateway != null)
+            internalStart(gateway, bundleContext);
         else
             bundleContext.addServiceListener(new ServiceListener()
             {
@@ -158,35 +127,22 @@ public class CallControlComponent
                     if (!(service instanceof SipGateway))
                         return;
 
-                    CallControlComponent.this.gateway = (SipGateway) service;
                     bundleContext.removeServiceListener(this);
-                    internalStart(bundleContext);
+                    internalStart((SipGateway) service, bundleContext);
                 }
             });
     }
 
-    private void internalStart(BundleContext bundleContext)
+    /**
+     * Creates new call control instance.
+     * @param gateway the SipGateway instance.
+     * @param bundleContext the osgi context for this bundle.
+     */
+    private void internalStart(SipGateway gateway, BundleContext bundleContext)
     {
-        ConfigurationService config
-            = ServiceUtils.getService(
-            bundleContext, ConfigurationService.class);
-
-        Boolean always_trust_mode = config.getBoolean(
-            "net.java.sip.communicator.service.gui.ALWAYS_TRUST_MODE_ENABLED",false);
-        if (always_trust_mode)
-        {
-            // Always trust mode - prevent failures because there's no GUI
-            // to ask the user, but do we always want to trust so, in this
-            // mode, the service is vulnerable to Man-In-The-Middle attacks.
-            logger.warn("Always trust in remote TLS certificates mode is enabled");
-        }
-
-        this.allowedJid = config.getString(ALLOWED_JID_P_NAME, null);
-
-        if (allowedJid != null)
-        {
-            logger.info("JID allowed to make outgoing calls: " + allowedJid);
-        }
+        this.callControl = new CallControl(
+            gateway,
+            ServiceUtils.getService(bundleContext, ConfigurationService.class));
     }
 
     @Override
@@ -220,23 +176,6 @@ public class CallControlComponent
     public String getName()
     {
         return "Call control";
-    }
-
-    /**
-     * Initializes new outgoing call.
-     * @param roomName the name of the MUC room that holds JVB conference call.
-     * @param roomPass optional password required to enter MUC room.
-     * @param from source address(optional)
-     * @param to destination call address/URI.
-     * @return the call resource string that will identify newly created call.
-     */
-    String initNewCall(String roomName, String roomPass, String from, String to)
-    {
-        String callResource = Util.generateNextCallResource();
-
-        gateway.createOutgoingCall(to, roomName, roomPass, callResource);
-
-        return callResource;
     }
 
     /**
@@ -276,84 +215,19 @@ public class CallControlComponent
     public IQ handleIQ(IQ iq)
         throws Exception
     {
-        try
+        org.jivesoftware.smack.packet.IQ resultIQ = null;
+        if (callControl == null)
         {
-            org.jivesoftware.smack.packet.IQ smackIq = IQUtils.convert(iq);
-
-            String fromBareJid = iq.getFrom().toBareJID();
-            if (allowedJid != null && !allowedJid.equals(fromBareJid))
-            {
-                org.jivesoftware.smack.packet.IQ error
-                    = org.jivesoftware.smack.packet.IQ
-                        .createErrorResponse(
-                                smackIq,
-                                new XMPPError(XMPPError.Condition.not_allowed));
-
-                return IQUtils.convert(error);
-            }
-            else if (allowedJid == null)
-            {
-                logger.warn("Requests are not secured by JID filter!");
-            }
-
-            if (smackIq instanceof RayoIqProvider.DialIq)
-            {
-                RayoIqProvider.DialIq dialIq = (RayoIqProvider.DialIq) smackIq;
-
-                String from = dialIq.getSource();
-                String to = dialIq.getDestination();
-
-                String roomName = dialIq.getHeader(ROOM_NAME_HEADER);
-                String roomPassword = dialIq.getHeader(ROOM_PASSWORD_HEADER);
-                if (roomName == null)
-                    throw new RuntimeException("No JvbRoomName header found");
-
-                logger.info(
-                    "Got dial request " + from + " -> " + to
-                    + " room: " + roomName);
-
-                String callResource
-                    = initNewCall(roomName, roomPassword, from, to);
-
-                callResource = "xmpp:" + callResource;
-
-                RayoIqProvider.RefIq ref
-                    = RayoIqProvider.RefIq.createResult(smackIq, callResource);
-
-                return IQUtils.convert(ref);
-            }
-            else if (smackIq instanceof RayoIqProvider.HangUp)
-            {
-                RayoIqProvider.HangUp hangUp
-                    = (RayoIqProvider.HangUp) smackIq;
-
-                String callUri = hangUp.getTo();
-                String callResource = callUri;
-
-                GatewaySession session = gateway.getSession(callResource);
-
-                if (session == null)
-                    throw new RuntimeException(
-                        "No gateway for call: " + callResource);
-
-                //hangupMap.put(gateway, smackIq.getFrom());
-
-                session.hangUp();
-
-                org.jivesoftware.smack.packet.IQ result
-                    = org.jivesoftware.smack.packet.IQ.createResultIQ(smackIq);
-
-                return IQUtils.convert(result);
-            }
-            else
-            {
-                return super.handleIQSet(iq);
-            }
+            logger.warn("Call controller not initialized");
         }
-        catch (Exception e)
+        else
         {
-            logger.error(e, e);
-            throw e;
+            resultIQ = callControl.handleIQ(IQUtils.convert(iq));
         }
+
+        if (resultIQ != null)
+            return IQUtils.convert(resultIQ);
+
+        return super.handleIQSet(iq);
     }
 }
