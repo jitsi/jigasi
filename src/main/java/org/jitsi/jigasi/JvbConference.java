21144df588103a39de41f5621e3fa9f2a503de2f
Nik <nikvaessen@users.noreply.github.com>
2018-08-02 16:50:01 +0200

    only let transcriber use hidden domain when joining a conference (#121)
    * only let transcriber use hidden domain when joining a conference
    
    * Revert "only let transcriber use hidden domain when joining a conference"
    
    This reverts commit ff296ca9e39c6bd5f6f910d35dfa066dac35efad.
    
    * use a SIP account property which can disable using auth login
    
    * attempt at disabling auth login for sip by not putting username and pass
    
    * deal with ANONYMOUS_AUTH property when PREVENT_AUTH_LOGIN=true

--- src/main/java/org/jitsi/jigasi/JvbConference.java
+++ src/main/java/org/jitsi/jigasi/JvbConference.java
@@ -1093,6 +1093,14 @@ public class JvbConference
         properties.put(ProtocolProviderFactory.ENCRYPTION_PROTOCOL_STATUS
             + ".DTLS-SRTP", "true");
 
+        AbstractGateway gw = gatewaySession.getGateway();
+        boolean preventAuthLogin = false;
+        if(gw instanceof SipGateway)
+        {
+            preventAuthLogin = Boolean.valueOf(
+                ((SipGateway) gw).getSipAccountProperty("PREVENT_AUTH_LOGIN"));
+        }
+
         String overridePrefix = "org.jitsi.jigasi.xmpp.acc";
         List<String> overriddenProps =
             JigasiBundleActivator.getConfigurationService()
@@ -1105,7 +1113,8 @@ public class JvbConference
 
             // The key for the password field can't end in PASSWORD, otherwise
             // it is encrypted by our configuration service implementation.
-            if ("org.jitsi.jigasi.xmpp.acc.PASS".equals(overridenProp))
+            if ("org.jitsi.jigasi.xmpp.acc.PASS".equals(overridenProp) &&
+                !preventAuthLogin)
             {
                 // The password is fully managed (i.e. stored/retrieved) by the
                 // configuration service and credentials storage service. See
@@ -1142,6 +1151,23 @@ public class JvbConference
                 // account and there we can alter the connection credentials.
 
                 this.xmppPassword = value;
+            }
+            else if ("org.jitsi.jigasi.xmpp.acc.USER_ID".equals(overridenProp)
+                && !preventAuthLogin)
+            {
+                properties.put(key, value);
+            }
+            else if ("org.jitsi.jigasi.xmpp.acc.ANONYMOUS_AUTH"
+                .equals(overridenProp))
+            {
+                if (preventAuthLogin)
+                {
+                    properties.put(key, Boolean.toString(true));
+                }
+                else
+                {
+                    properties.put(key, value);
+                }
             }
             else if ("org.jitsi.jigasi.xmpp.acc.BOSH_URL_PATTERN"
                         .equals(overridenProp))
