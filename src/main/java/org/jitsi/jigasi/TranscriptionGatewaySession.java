jitsi/jigasi

commit 4d6b8bfaeff5d30a91c485ea11ad1d4e9e4710ab

Moves acceptCall in gwSessions.
Change the way we answer outgoing calls and call setConference. Do it
same way as incoming calls, first do setConference and then acceptCall.

src/main/java/org/jitsi/jigasi/TranscriptionGatewaySession.java
@@ -160,6 +160,8 @@ public class TranscriptionGatewaySession
         });
 
         logger.debug("Invited for conference");
+
+        CallManager.acceptCall(incomingCall);
     }
 
     @Override

src/main/java/org/jitsi/jigasi/JvbConference.java
@@ -584,11 +584,16 @@ public class JvbConference
     {
         // Advertise gateway feature before joining
         addSupportedFeatures(
-                xmppProvider.getOperationSet(OperationSetJitsiMeetTools.class));
+            xmppProvider.getOperationSet(OperationSetJitsiMeetTools.class));
 
         OperationSetMultiUserChat muc
             = xmppProvider.getOperationSet(OperationSetMultiUserChat.class);
         muc.addPresenceListener(this);
+
+        OperationSetIncomingDTMF opSet
+            = this.xmppProvider.getOperationSet(OperationSetIncomingDTMF.class);
+        if (opSet != null)
+            opSet.addDTMFListener(gatewaySession);
 
         try
         {
@@ -719,24 +724,6 @@ public class JvbConference
         if(started)
         {
             stop();
-        }
-    }
-
-    private void onJvbCallStarted()
-    {
-        logger.info("JVB conference call IN_PROGRESS "
-            + callContext.getRoomName());
-
-        OperationSetIncomingDTMF opSet
-            = this.xmppProvider.getOperationSet(OperationSetIncomingDTMF.class);
-        if (opSet != null)
-            opSet.addDTMFListener(gatewaySession);
-
-        Exception error = gatewaySession.onConferenceCallStarted(jvbCall);
-
-        if (error != null)
-        {
-            logger.error(error, error);
         }
     }
 
@@ -1012,9 +999,6 @@ public class JvbConference
             jvbCall.addCallChangeListener(statsHandler);
 
             gatewaySession.onConferenceCallInvited(jvbCall);
-
-            // Accept incoming jingle call
-            CallManager.acceptCall(jvbCall);
         }
 
         @Override
@@ -1038,12 +1022,7 @@ public class JvbConference
                 return;
             }
 
-            // Once call is started notify SIP gateway
-            if (jvbCall.getCallState() == CallState.CALL_IN_PROGRESS)
-            {
-                onJvbCallStarted();
-            }
-            else if(jvbCall.getCallState() == CallState.CALL_ENDED)
+            if(jvbCall.getCallState() == CallState.CALL_ENDED)
             {
                 onJvbCallEnded();
             }

src/main/java/org/jitsi/jigasi/SipGatewaySession.java
@@ -407,6 +407,16 @@ public class SipGatewaySession
                 });
             }
         }
+
+        logger.info("JVB conference call IN_PROGRESS "
+            + callContext.getRoomName());
+
+        Exception error = this.onConferenceCallStarted(incomingCall);
+
+        if (error != null)
+        {
+            logger.error(error, error);
+        }
     }
 
     /**
@@ -491,6 +501,8 @@ public class SipGatewaySession
                 return e;
             }
         }
+
+        CallManager.acceptCall(jvbConferenceCall);
 
         return null;
     }

commit 06d6e370d30df8180e26834fd5e22c30d095531f

Fixes initiating transcriber.
Transcription was broken after the changes in
4d6b8bfaeff5d30a91c485ea11ad1d4e9e4710ab.

src/main/java/org/jitsi/jigasi/TranscriptionGatewaySession.java
@@ -159,9 +159,15 @@ public class TranscriptionGatewaySession
             }
         });
 
-        logger.debug("Invited for conference");
+        logger.info("JVB conference call IN_PROGRESS "
+            + callContext.getRoomName());
 
-        CallManager.acceptCall(incomingCall);
+        Exception error = this.onConferenceCallStarted(incomingCall);
+
+        if (error != null)
+        {
+            logger.error(error, error);
+        }
     }
 
     @Override
@@ -219,6 +225,8 @@ public class TranscriptionGatewaySession
         {
             sendMessageToRoom(welcomeMessage.toString());
         }
+
+        CallManager.acceptCall(jvbConferenceCall);
 
         logger.debug("TranscriptionGatewaySession started transcribing");
 