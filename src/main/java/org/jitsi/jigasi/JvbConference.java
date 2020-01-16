0b4e0a3cf22ac2cd6d5b28f3382948a9e6ce52f4
damencho <damencho@jitsi.org>
2020-01-16 12:19:54 +0000

    Prints bosh sessionId if available.
    Useful for debugging.

--- src/main/java/org/jitsi/jigasi/JvbConference.java
+++ src/main/java/org/jitsi/jigasi/JvbConference.java
@@ -1,7 +1,7 @@
 /*
  * Jigasi, the JItsi GAteway to SIP.
  *
- * Copyright @ 2015 Atlassian Pty Ltd
+ * Copyright @ 2018 - present 8x8, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,6 +36,7 @@ import org.jitsi.service.neomedia.*;
 import org.jitsi.utils.*;
 import org.jitsi.xmpp.extensions.rayo.*;
 import org.jivesoftware.smack.*;
+import org.jivesoftware.smack.bosh.*;
 import org.jivesoftware.smack.packet.*;
 import org.jivesoftware.smackx.nick.packet.*;
 import org.jxmpp.jid.*;
@@ -44,6 +45,7 @@ import org.jxmpp.jid.parts.*;
 import org.jxmpp.stringprep.*;
 import org.osgi.framework.*;
 
+import java.lang.reflect.*;
 import java.util.*;
 
 import static org.jivesoftware.smack.packet.XMPPError.Condition.*;
@@ -569,6 +571,20 @@ public class JvbConference
         {
             // Join the MUC
             joinConferenceRoom();
+
+            if (xmppProvider != null &&
+                ((ProtocolProviderServiceJabberImpl) xmppProvider)
+                    .getConnection() instanceof XMPPBOSHConnection)
+            {
+                Object sessionId = getConnSessionId(
+                    ((ProtocolProviderServiceJabberImpl) xmppProvider)
+                        .getConnection());
+                if (sessionId != null)
+                {
+                    logger.error(this.callContext + " Registered bosh sid: "
+                        + sessionId);
+                }
+            }
         }
         else if (evt.getNewState() == RegistrationState.UNREGISTERED)
         {
@@ -1544,6 +1560,59 @@ public class JvbConference
                 + endReason + ","
                 + errorLog + "]@"
                 + hashCode();
+        }
+    }
+
+    /**
+     * Extracts bosh connection sessionId, we just print it for debug
+     * purposes.
+     * @param connection the bosh connection which sessionId we will try to
+     * extract.
+     * @return the sessionId if extracted or null.
+     */
+    private static Object getConnSessionId(Object connection)
+    {
+        Field myField = getField(XMPPBOSHConnection.class, "sessionID");
+
+        if (myField != null)
+        {
+            myField.setAccessible(true);
+            try
+            {
+                return myField.get(connection);
+            }
+            catch( Exception e)
+            {
+                logger.error("cannot read it", e);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Utility method to get the field from a class.
+     * @param clazz the class.
+     * @param fieldName the field men.
+     * @return the field or null.
+     */
+    private static Field getField(Class clazz, String fieldName)
+    {
+        try
+        {
+            return clazz.getDeclaredField(fieldName);
+        }
+        catch (NoSuchFieldException e)
+        {
+            Class superClass = clazz.getSuperclass();
+            if (superClass == null)
+            {
+                return null;
+            }
+            else
+            {
+                return getField(superClass, fieldName);
+            }
         }
     }
 }
