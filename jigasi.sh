b1cb89762643f621e024232bb192b34ab628aebd
damencho <damencho@jitsi.org>
2020-01-22 10:28:45 +0000

    Removes ant and update scripts for creating debian package.

--- jigasi.sh
+++ jigasi.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-kernel="$(uname -s)"
-if [ $kernel == "Darwin" ] ; then
-    SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-    architecture="macosx"
-elif [ $kernel == "Linux" ] ; then
-    SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-    architecture="linux"
-
-    machine="$(uname -m)"
-    if [ "$machine" == "x86_64" ] || [ "$machine" == "amd64" ] ; then
-        architecture=$architecture"-64"
-    fi
-else
-    echo "Unknown system : $kernel"
-    exit 1
-fi
-
 mainClass="org.jitsi.jigasi.Main"
 cp=$(JARS=($SCRIPT_DIR/jigasi.jar $SCRIPT_DIR/lib/*.jar); IFS=:; echo "${JARS[*]}")
-libs="$SCRIPT_DIR/lib/native/$architecture"
+libs="$SCRIPT_DIR/lib/native"
 logging_config="$SCRIPT_DIR/lib/logging.properties"
 
 # if there is a logging config file in lib folder use it (running from source)
