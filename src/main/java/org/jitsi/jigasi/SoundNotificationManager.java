54128235217022406af1d56da66be816c344b6c7
damencho <damencho@jitsi.org>
2020-09-15 15:14:52 -0500

    squash: Further improve sending audio notifications.
    Make sure we not over sleep and send the first 200ms directly. Thanks to @bgrozev.

--- src/main/java/org/jitsi/jigasi/SoundNotificationManager.java
+++ src/main/java/org/jitsi/jigasi/SoundNotificationManager.java
@@ -357,7 +357,8 @@ public class SoundNotificationManager
         long ts = new Random().nextInt(0xFFFF);
         long ssrc = new Random().nextInt(0xFFFF);
         byte pt = stream.getDynamicRTPPayloadType(Constants.OPUS);
-        long timePushPreviousData = 0;
+        long timeForNextPacket = System.currentTimeMillis();
+        long sentDuration = 0;
 
         while ((opusAudioData = of.getNextAudioPacket()) != null)
         {
@@ -386,14 +387,14 @@ public class SoundNotificationManager
 
             System.arraycopy(
                 data, 0, rtp.getBuffer(), rtp.getPayloadOffset(), data.length);
-
-            long sleepTime = nSamples/48 - (System.currentTimeMillis() - timePushPreviousData);
-            if (sleepTime < 0)
-                sleepTime = 0;
-            Thread.sleep(sleepTime);
-
+            int duration = nSamples/48;
+            timeForNextPacket += duration;
+            sentDuration += duration;
             stream.injectPacket(rtp, true, null);
-            timePushPreviousData = System.currentTimeMillis();
+            long sleep = timeForNextPacket - System.currentTimeMillis();
+            if (sleep > 0 && sentDuration > 200) { // we let the first 200ms to be sent without waiting
+                Thread.sleep(sleep);
+            }
         }
     }
 
