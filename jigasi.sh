04ad08f0ab9cd079c62f47f9cecc7367ce3f4ba4
Дамян Минков <damencho@jitsi.org>
2024-03-20 08:31:07 -0500

    * feat: Adds java 17 dependencies.  (#529)
    * feat: Adds java 17 dependencies.
    
    Sets to use old DatagramSocket implementation.
    
    * squash: Fixes loading native libraries.

--- jigasi.sh
+++ jigasi.sh
@@ -22,4 +22,4 @@ fi
 
 if [ -z "$JIGASI_MAX_MEMORY" ]; then JIGASI_MAX_MEMORY=3072m; fi
 
-LD_LIBRARY_PATH=$libs exec java -Xmx$JIGASI_MAX_MEMORY -Djava.library.path=$libs $LOGGING_CONFIG_PARAM $JAVA_SYS_PROPS -cp $cp $mainClass $@
+LD_LIBRARY_PATH=$libs exec java -Xmx$JIGASI_MAX_MEMORY -Djdk.net.usePlainDatagramSocketImpl=true -Djava.library.path=$libs $LOGGING_CONFIG_PARAM $JAVA_SYS_PROPS -cp $cp $mainClass $@
